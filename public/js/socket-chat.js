071a07bfff5ed175848909e57846f14909469a6f
camiloadominguez <camiloadoming@hotmail.com>
2020-05-20 17:05:30 -0500

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,47 +1,61 @@
-let socket = io();
+var socket = io();
 
-let params = new URLSearchParams(window.location.search);
-if(!params.has('nombre')|| !params.has('sala')){
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El parametro nombre y sala son requeridos');
+    throw new Error('El nombre y sala son necesarios');
 }
 
-let usuario = {
+var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
-// las funciones on escuchan eventos
-socket.on('connect', function (){
-    console.log('conectado al servidor');
-    socket.emit('entrarChat', usuario, (resp)=>{
-        console.log('Usuarios conectados:', resp);
-    })
+};
+
+
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
+    });
+
 });
-socket.on('disconnect', function(){
-    console.log('Se perdio la conexion con el servidor');
-})
 
-//emitir un mensaje desde el frontend para que el servidor lo escuche
-// las funciones emmit envian informacion
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Camilo Dominguez',
-//     mensaje: 'Hola CADS'
-// }, (resp)=>{
-//     console.log('respuesta del servidor', resp);
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
 // });
-//escuchar información
 
-socket.on('crearMensaje',(resp)=>{
-    console.log('Respuesta del servidor',resp);
-})
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+    rendrizarMensajes(mensaje,false);
+    scrollBottom();
+});
 
-//escuchar cuando un usuario entra o sale del chat
-socket.on('listaPersonas',(personas)=>{
-    console.log('Personas conectadas en el momento',personas);
-})
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    console.log(personas)
+    renderizarUsuarios(personas);
+});
 
-//mensaje privados
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
 
-socket.on('mensajePrivado', (mensaje)=>{
-    console.log('mensajePrivado', mensaje)
-})
+    console.log('Mensaje Privado:', mensaje);
+
+});
--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -17,22 +17,25 @@ io.on('connection',(client) => {
     client.join(usuario.sala);
 
         let personas = usuarios.agregarPersona(client.id, usuario.nombre, usuario.sala )
-        client.broadcast.to(usuario.sala).emit('listaPersonas', usuarios.getPersonaPorSala(usuario.sala));
+        client.broadcast.to(usuario.sala).emit('listaPersona', usuarios.getPersonaPorSala(usuario.sala));
+        client.broadcast.to(usuario.sala).emit('crearMensaje', crearMensaje('admin',`${usuario.nombre} se unió`));
         callback(usuarios.getPersonaPorSala(usuario.sala));
     });
 
-    client.on('crearMensaje', (data)=>{
+    client.on('crearMensaje', (data, callback)=>{
 
         let persona = usuarios.getPersona(client.id);
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
     });
 
     client.on('disconnect', ()=>{
         let personaBorrada = usuarios.borrarPersona(client.id)
 
         client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('admin',`${personaBorrada.nombre} se desconecto`));
-        client.broadcast.to(personaBorrada.sala).emit('listaPersonas', usuarios.getPersonaPorSala(personaBorrada.sala));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonaPorSala(personaBorrada.sala));
     })
 
     //mensajes privados
